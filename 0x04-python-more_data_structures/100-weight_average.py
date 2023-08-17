44b4335a2d4e28b8f3f667995bca4fe8fca5058f
Martin Kihungi <mwangi4786@gmail.com>
2023-08-17 03:08:56 -0700

    100-weight_average.py correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    cummulatineNo = 0
-    cummulativeDiv = 0
-    for eachTuplePair in my_list:
-        cummulatineNo += (eachTuplePair[0] * eachTuplePair[1])
-        cummulativeDiv += eachTuplePair[1]
-    return cummulatineNo / cummulativeDiv
-
+    if len(my_list) > 0:
+        cummulatineNo = 0
+        cummulativeDiv = 0
+        for eachTuplePair in my_list:
+            cummulatineNo += (eachTuplePair[0] * eachTuplePair[1])
+            cummulativeDiv += eachTuplePair[1]
+        return cummulatineNo / cummulativeDiv
+    else:
+        return 0

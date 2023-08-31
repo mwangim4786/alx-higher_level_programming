b8d136e42671cb59d593814b194eef396b0640c0
MMK <mwangim4786@gmail.com>
2023-08-31 14:13:43 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module to define class Square """
+
+
+class Square:
+    """ Created empty class Square 
+    
+    Attribures:
+    __size
+    """
+    def __init__(self, size):
+        """Initializes a square
+        Args:
+            size (int): size of a side of the square
+        Returns: None
+        """
+        self.__size = size

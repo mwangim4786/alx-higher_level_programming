786cca1811c3ecb654c75ba2a093ed5c4ba734e8
MMK <mwangim4786@gmail.com>
2023-09-12 17:08:28 +0300

    8-class_to_json.py correction

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-Contains the "class_to_json" function
+Contains the "class_to_json" function that returns the dictionary
+description with simple data structure (list, dictionary, string,
+integer and boolean) for JSON serialization of an object
 """
 
 

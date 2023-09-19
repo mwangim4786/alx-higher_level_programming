2dec0530a4a37c352ccd53ea83b558c6f28b4271
MMK <mwangim4786@gmail.com>
2023-09-19 15:56:55 +0300

    JSON

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
+
+
 
 class Base:
     """Represent the base model.
@@ -21,6 +24,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of dictionaries.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
 
 
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -155,3 +155,18 @@ class Rectangle(Base):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Return a dictionary representation of Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+
+
+
+

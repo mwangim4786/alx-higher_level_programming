a2115638ec9b76a17e987f1400e08896a91c0748
Martin Kihungi <mwangi4786@gmail.com>
2023-11-15 09:48:11 -0800

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+	charPrint(c){
+		if (c === undefined) {
+			c = 'X';
+		}
+		for (let i = 0; i < this.height; i++) {
+			let row = '';
+			for (let j = 0; j < this.width; j++)
+			row += c;
+			console.log(row);
+		}
+	}
+}
+
+module.exports = Square;

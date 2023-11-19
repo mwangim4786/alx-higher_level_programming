519069660dde4f21589bcfe8004b4873ced2b4a2
Martin Kihungi <mwangi4786@gmail.com>
2023-11-19 12:41:01 -0800

    First

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to list all states from sql db"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    # Connect to mysql server
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+
+    # Sql query to fetch state as specified in argument[4]
+    cur.execute("SELECT * FROM states WHERE `name` = '{}'".format(sys.argv[4]))
+
+    # Printing the data
+    for state in cur.fetchall():
+        print(state)

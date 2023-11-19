1db9c4c3c892f8cd50f4140d057166914f1d793a
Martin Kihungi <mwangi4786@gmail.com>
2023-11-19 12:50:26 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,11 +4,11 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    # Connect to mysql server
+    """Connect to mysql server"""
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
 
-    # Sql query to fetch state as specified in argument[4]
+    """Sql query to fetch state as specified in argument[4]"""
     cur.execute("SELECT * FROM states WHERE `name` = '{}'".format(sys.argv[4]))
 
     # Printing the data

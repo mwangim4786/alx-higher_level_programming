954f5c503e2e30b973b0310f493959108453577f
Martin Kihungi <mwangi4786@gmail.com>
2023-12-01 13:12:05 -0800

    1-hbtn_header.py 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alx-intranet.hbtn.io/status."""
+"""Displays the X-Request-Id header variable of a request to a given URL."""
 
 import sys
 import urllib.request

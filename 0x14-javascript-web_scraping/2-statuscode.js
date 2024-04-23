0c8453ce12c150036bf405ee105179ffd79bab76
mwangim4786 <mwangim4786@gmail.com>
2024-04-23 07:42:18 -0700

    2-statuscode.js edit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const request = require('request');
 // Import the request module.
 
-request.get(process.argv[2]).on('response', function (response){
+request.get(process.argv[2]).on('response', function (response) {
 // Use request module to perform HTTP request to the url.
 // event listener for the response event.
 

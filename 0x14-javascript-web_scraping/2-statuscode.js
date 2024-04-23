926e4356464bf3df9eed5b33c4c4d904042712b0
mwangim4786 <mwangim4786@gmail.com>
2024-04-23 14:46:01 -0700

    2-statuscode.js 10

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,6 @@ const request = require('request');
 
 request.get(process.argv[2]).on('response', function (response) {
 // Use request module to perform HTTP request to the url.
-// event listener for the response event.
 
   console.log('code: ${response.statusCode}');
   // print the HTTP status code to the console.

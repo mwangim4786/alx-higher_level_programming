f3b8e0c89dcac9bb760c2270babf351e0ffd2df4
mwangim4786 <mwangim4786@gmail.com>
2024-04-23 07:40:30 -0700

    all

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content){
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
   if (error) {
     console.error('Error reading the file:', error);
   } else {
